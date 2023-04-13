f8e15334053959bfa80f5e28b4dadb5bb6bf9aab
dingtom <2524370217@qq.com>
2023-04-14 01:09:48 +0800

    add cbam2

--- train.py
+++ train.py
@@ -443,7 +443,7 @@ def parse_opt(known=False):
     parser.add_argument('--amp', action='store_true', help='use amp')
     parser.add_argument('--cfg', type=str, default='models/yolov5s.yaml', help='model.yaml path')
     # parser.add_argument('--data', type=str, default=ROOT / 'data/zhongliu.yaml', help='dataset.yaml path')
-    # parser.add_argument('--data', type=str, default=ROOT / 'data/hand.yaml', help='dataset.yaml path')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/hand.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-high.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=200, help='total training epochs')
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
